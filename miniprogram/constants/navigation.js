dc5acd4937263320d797c80371b79295486470f3
第九 <2331613886@qq.com>
2026-05-12 11:29:54 +0800

    修改了部分UI显示异常问题

--- miniprogram/constants/navigation.js
+++ miniprogram/constants/navigation.js
@@ -2,27 +2,23 @@ const { PAGE_ROUTES } = require("./config");
 
 const PRIMARY_CHANNELS = [
   {
-    key: "treehole",
-    label: "树洞",
-    icon: "树",
-    route: PAGE_ROUTES.TREEHOLE_FEED,
-  },
-  {
     key: "assessment",
     label: "测评",
-    icon: "测",
     route: PAGE_ROUTES.HOME,
   },
   {
     key: "report",
     label: "报告",
-    icon: "报",
     route: PAGE_ROUTES.REPORT_SUMMARY,
+  },
+  {
+    key: "treehole",
+    label: "树洞",
+    route: PAGE_ROUTES.TREEHOLE_FEED,
   },
   {
     key: "my",
     label: "我的",
-    icon: "我",
     route: PAGE_ROUTES.PROFILE,
   },
 ];
